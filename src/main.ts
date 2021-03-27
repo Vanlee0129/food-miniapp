4576fe285bb37807ddf80354952f1d3087baa291
Vanlee0129 <33856668+Vanlee0129@users.noreply.github.com>
2021-03-27 15:25:23 +0800

    feat: vue3 set

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import Vue from 'vue'
+import uView from "uview-ui";
+import VueCompositionAPI from '@vue/composition-api'
 import App from './App.vue'
+
+Vue.use(uView);
+Vue.use(VueCompositionAPI);
 
 Vue.config.productionTip = false
 
-new App().$mount()
+const app: Vue = new App()
+
+app.$mount()
+
